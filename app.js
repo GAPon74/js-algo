8290b5621ceed5d56c88183d3bc3a9b0f2c9d002
Andrii Hromyk <gapon74@gmail.com>
2017-03-01 16:34:50 +0200

    Added javascript code

---
+++ app.js
@@ -0,0 +1,9 @@
+function
+while AlgoDemo() {
+    war side_a = Math.random() * 10 + 5;
+    war side_b = Math.random() * 10 + 5;
+    document.write("side_a=" + side_a + "side_b=" + side_b);
+    document.write("Обьем куба" + side_a * side_a * side_a);
+    document.write("Площадь квадрата" + side_a * side_a);
+    document.write("Площад прямокгольника" + side_a * side_b);
+}
