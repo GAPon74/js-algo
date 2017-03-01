4f693f0f2487b8aad9992d034d5e2a251cd7bf36
Andrii Hromyk <gapon74@gmail.com>
2017-03-01 17:54:27 +0200

    Changes for next implementation of algorithms

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-function writeAlgoDemo() {
+function writeAlgoDemo1() {
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
     document.write("Side_a=" + side_a + "Side_b=" + side_b);
     document.write("Обьем куба" + side_a * side_a * side_a);
     document.write("Площадь квадрата" + side_a * side_a);
     document.write("Площад прямокгольника" + side_a * side_b);
+}
+
+function writeAlgoDemo2() {
+
 }
