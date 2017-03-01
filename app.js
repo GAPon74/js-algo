88a34f5d46e77473b620761aa1bdab10b1e1e5ea
Andrii Hromyk <gapon74@gmail.com>
2017-03-01 17:46:02 +0200

    Changes (view) in writ eAlgoDemo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function writeAlgoDemo() {
-    war side_a = Math.random() * 10 + 5;
-    war side_b = Math.random() * 10 + 5;
+    var side_a = Math.random() * 10 + 5;
+    var side_b = Math.random() * 10 + 5;
     document.write("Side_a=" + side_a + "Side_b=" + side_b);
     document.write("Обьем куба" + side_a * side_a * side_a);
     document.write("Площадь квадрата" + side_a * side_a);
