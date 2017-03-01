5d42e8c1dc6f4a2c40c0c389d3db605c5862cff1
Andrii Hromyk <gapon74@gmail.com>
2017-03-01 18:51:58 +0200

    end

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function writeAlgoDemo1() {
     var side_a = Math.random() * 10 + 5;
     var side_b = Math.random() * 10 + 5;
     document.write("<p>Side a= " + side_a + "</p><p>Side b= " + side_b + "</p>");
-    document.write("<p>Обьем куба равен " + side_a * side_a * side_a + "</p>");
+    document.write("<p>Обьем куба равна " + side_a * side_a * side_a + "</p>");
     document.write("<p>Площадь квадрата равна " + side_a * side_a + "</p>");
     document.write("<p>Площадь прямоугольника равна " + side_a * side_b + "</p>");
 }
